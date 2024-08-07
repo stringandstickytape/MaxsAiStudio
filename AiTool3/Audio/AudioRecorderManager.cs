64e1f9f1461ce62d52361113c1bef15845865a25
Max <max.hedge@gmail.com>
2024-08-07 22:43:34 +0100

    Tidy up.

--- AiTool3/Audio/AudioRecorderManager.cs
+++ AiTool3/Audio/AudioRecorderManager.cs
@@ -1,10 +1,5 @@
-﻿using System.Diagnostics;
-using Whisper.net.Ggml;
+﻿using Whisper.net.Ggml;
 using Whisper.net;
-using Whisper.net.Wave;
-using System.Text;
-using NAudio.Wave;
-using NAudio.MediaFoundation;
 using AiTool3.UI;
 
 namespace AiTool3.Audio
@@ -55,15 +50,6 @@ namespace AiTool3.Audio
 
         }
 
-
-
-
-        static async Task DownloadModel(string fileName, GgmlType ggmlType)
-        {
-            using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(ggmlType);
-            using var fileWriter = File.OpenWrite(fileName);
-            await modelStream.CopyToAsync(fileWriter);
-        }
         public async Task StopRecording()
         {
             if (cts != null)
@@ -87,37 +73,5 @@ namespace AiTool3.Audio
 
             return;
         }
-
-        private async Task<string> ProcessAudio()
-        {
-            // requires specific DLLs...
-
-            if (!File.Exists(modelName))
-            {
-                using var modelStream = await WhisperGgmlDownloader.GetGgmlModelAsync(ggmlType);
-                using var fileWriter = File.OpenWrite(modelName);
-                await modelStream.CopyToAsync(fileWriter);
-            }
-
-            var retVal = "";
-
-
-            using var whisperFactory = WhisperFactory.FromPath(modelName);
-
-            using var processor = whisperFactory.CreateBuilder()
-                .WithLanguage("auto")
-                .Build();
-
-            using var fileStream = File.OpenRead("output.wav");
-
-            await foreach (var result in processor.ProcessAsync(fileStream))
-            {
-                // write to output.txt
-                retVal = $"{retVal}{result.Text}";
-            }
-            return retVal;
-        }
-
-
     }
 }
