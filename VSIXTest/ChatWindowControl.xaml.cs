40e54e39680e14ce42137b09b840710eed3a3de6
Max <max.hedge@gmail.com>
2024-08-26 01:41:02 +0100

    More VSIX #

--- VSIXTest/ChatWindowControl.xaml.cs
+++ VSIXTest/ChatWindowControl.xaml.cs
@@ -1,13 +1,22 @@
-﻿using System.Windows;
+﻿using EnvDTE;
+using EnvDTE80;
+using Microsoft.VisualStudio.Shell;
+using System.Collections.Generic;
+using System;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Input;
+using System.IO;
 
 namespace VSIXTest
 {
     public partial class ChatWindowControl : UserControl
     {
+        private DTE2 _dte;
+
         public ChatWindowControl()
         {
+            _dte = Package.GetGlobalService(typeof(DTE)) as DTE2;
             InitializeComponent();
         }
 
@@ -24,12 +33,25 @@ namespace VSIXTest
         {
             if (InputTextBox.Text.EndsWith("#"))
             {
-                ShowShortcutMenu();
+                PopulateAndShowShortcutMenu();
             }
         }
 
-        private void ShowShortcutMenu()
+        private void PopulateAndShowShortcutMenu()
         {
+            ShortcutMenu.Items.Clear();
+            var files = GetAllFilesInSolution();
+            foreach (var file in files)
+            {
+                var menuItem = new MenuItem
+                {
+                    Header = $"#{Path.GetFileName(file)}",
+                    Tag = file
+                };
+                menuItem.Click += ShortcutMenuItem_Click;
+                ShortcutMenu.Items.Add(menuItem);
+            }
+
             var textBoxPosition = InputTextBox.GetRectFromCharacterIndex(InputTextBox.CaretIndex);
             ShortcutMenu.PlacementTarget = InputTextBox;
             ShortcutMenu.Placement = System.Windows.Controls.Primitives.PlacementMode.RelativePoint;
@@ -38,13 +60,67 @@ namespace VSIXTest
             ShortcutMenu.IsOpen = true;
         }
 
+        private List<string> GetAllFilesInSolution()
+        {
+            var files = new List<string>();
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (_dte.Solution != null)
+            {
+                foreach (Project project in _dte.Solution.Projects)
+                {
+                    GetProjectFiles(project, files);
+                }
+            }
+
+            return files;
+        }
+
+        private void GetProjectFiles(Project project, List<string> files)
+        {
+            ThreadHelper.ThrowIfNotOnUIThread();
+
+            if (project.Kind == ProjectKinds.vsProjectKindSolutionFolder)
+            {
+                foreach (ProjectItem item in project.ProjectItems)
+                {
+                    if (item.SubProject != null)
+                    {
+                        GetProjectFiles(item.SubProject, files);
+                    }
+                }
+            }
+            else
+            {
+                foreach (ProjectItem item in project.ProjectItems)
+                {
+                    if (item.Properties != null)
+                    {
+                        try
+                        {
+                            string filePath = item.Properties.Item("FullPath").Value.ToString();
+                            if (File.Exists(filePath))
+                            {
+                                files.Add(filePath);
+                            }
+                        }
+                        catch (Exception)
+                        {
+                            // Handle or log any exceptions
+                        }
+                    }
+                }
+            }
+        }
+
         private void ShortcutMenuItem_Click(object sender, RoutedEventArgs e)
         {
             if (sender is MenuItem menuItem)
             {
                 int caretIndex = InputTextBox.CaretIndex;
-                InputTextBox.Text = InputTextBox.Text.Insert(caretIndex, menuItem.Header.ToString());
-                InputTextBox.CaretIndex = caretIndex + menuItem.Header.ToString().Length;
+                string insertText = menuItem.Header.ToString();
+                InputTextBox.Text = InputTextBox.Text.Insert(caretIndex, insertText);
+                InputTextBox.CaretIndex = caretIndex + insertText.Length;
                 ShortcutMenu.IsOpen = false;
             }
         }
