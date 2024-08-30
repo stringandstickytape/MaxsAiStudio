9ddecfc21760c4442a00e40030599c7ca205df77
Max <max.hedge@gmail.com>
2024-08-30 21:18:07 +0100

    .

---
+++ reinstallVsix.bat
@@ -0,0 +1,5 @@
+"C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\VSIXInstaller.exe" /uninstall:VSIXTest.c08e6e3d-480b-4568-889c-1d670bd07206
+pause
+C:\Users\maxhe\source\repos\CloneTest\MaxsAiTool\VSIXTest\bin\Debug\VSIXTest.vsix
+
+
