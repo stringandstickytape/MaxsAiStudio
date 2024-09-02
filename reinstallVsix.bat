7bc6d5384d2a7858d53093218898453c31a04ae1
Max <max.hedge@gmail.com>
2024-09-02 16:42:32 +0100

    Rewrite VSIX install script.

--- reinstallVsix.bat
+++ reinstallVsix.bat
@@ -1,4 +1,3 @@
 "C:\Program Files\Microsoft Visual Studio\2022\Professional\Common7\IDE\VSIXInstaller.exe" /uninstall:VSIXTest.c08e6e3d-480b-4568-889c-1d670bd07206
-C:\Users\maxhe\source\repos\CloneTest\MaxsAiTool\VSIXTest\bin\Debug\VSIXTest.vsix
-
+.\VSIXTest\bin\Debug\VSIXTest.vsix
 
