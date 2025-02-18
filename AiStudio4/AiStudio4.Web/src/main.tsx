bfb042429b6cf268b36cda22721479518546158f
Max <max.hedge@gmail.com>
2025-02-18 22:58:52 +0000

    .

--- AiStudio4/AiStudio4.Web/src/main.tsx
+++ AiStudio4/AiStudio4.Web/src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import App from './App.tsx'
+import './globals.css'  // Change this from index.css to globals.css
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
     <React.StrictMode>
