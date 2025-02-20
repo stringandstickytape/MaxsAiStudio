f78e86be6cbbd165a5fe0d8bedab3d48909f1a6d
Max <max.hedge@gmail.com>
2025-02-20 22:01:26 +0000

    Create v4BranchedConversation

--- AiStudio4/InjectedDependencies/ChatManager.cs
+++ AiStudio4/InjectedDependencies/ChatManager.cs
@@ -5,6 +5,7 @@ using Microsoft.Extensions.Configuration;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
+using System.IO;
 
 namespace AiStudio4.InjectedDependencies
 {
@@ -12,6 +13,11 @@ namespace AiStudio4.InjectedDependencies
     {
         private readonly SettingsManager _settingsManager;
         private readonly WebSocketServer _webSocketServer;
+
+        // usefulPath = Path.Combine(
+        // Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+        //        "AiStudio4",
+        //        filename);
 
         public ChatManager(SettingsManager settingsManager, WebSocketServer webSocketServer)
         {
@@ -21,50 +27,17 @@ namespace AiStudio4.InjectedDependencies
 
         public async Task<string> HandleChatRequest(string clientId, JObject requestObject)
         {
-            var conversationCacheManager = new ConversationCacheManager();
-
-            foreach (var conv in conversationCacheManager.Conversations)
-            {
-                await _webSocketServer.SendToClientAsync(clientId,
-                    JsonConvert.SerializeObject(new
-                    {
-                        messageType = "cachedconversation",
-                        content = new
-                        {
-                            id = conv.Value.ConvGuid,
-                            content = conv.Value.Summary.Length > 20 ? conv.Value.Summary.Substring(0,20) : conv.Value.Summary,
-                            source = "ai",
-                            parentId = (string)null,
-                            timestamp = new DateTimeOffset(conv.Value.LastModified).ToUnixTimeMilliseconds(),
-                            children = new string[] { }
-                        }
-                    }));
-            }
-
+            var conversationId = (string)requestObject["conversationId"]; // eg conv_1740088070013
             var model = _settingsManager.CurrentSettings.ModelList.First(x => x.ModelName == (string)requestObject["model"]);
             var userMessage = (string)requestObject["message"];
-            var parentId = $"msg_{Guid.NewGuid()}";
-
-            await _webSocketServer.SendToClientAsync(clientId,
-                JsonConvert.SerializeObject(new
-                {
-                    messageType = "conversation",
-                    content = new
-                    {
-                        id = parentId,
-                        content = userMessage.Length > 20 ? userMessage.Substring(0, 20) : userMessage,
-                        source = "user",
-                        parentId = (string)null,
-                        timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
-                        children = new string[] { }
-                    }
-                }));
 
             var service = ServiceProvider.GetProviderForGuid(_settingsManager.CurrentSettings.ServiceProviders, model.ProviderGuid);
             var aiService = AiServiceResolver.GetAiService(service.ServiceName, null);
 
             aiService.StreamingTextReceived += (sender, text) => AiService_StreamingTextReceived(clientId, text);
             aiService.StreamingComplete += (sender, text) => AiService_StreamingCompleted(clientId, text);
+
+            var v4conversation = LoadOrCreateConversation(conversationId);
 
             var conversation = new Conversation(DateTime.Now)
             {
@@ -78,21 +51,6 @@ namespace AiStudio4.InjectedDependencies
                 service, model, conversation, null!, null!,
                 new CancellationToken(false), _settingsManager.CurrentSettings,
                 mustNotUseEmbedding: true, toolNames: null, useStreaming: true);
-
-            await _webSocketServer.SendToClientAsync(clientId,
-                JsonConvert.SerializeObject(new
-                {
-                    messageType = "conversation",
-                    content = new
-                    {
-                        id = $"msg_{Guid.NewGuid()}",
-                        content = response.ResponseText,
-                        source = "ai",
-                        parentId = parentId,
-                        timestamp = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds(),
-                        children = new string[] { }
-                    }
-                }));
 
             return JsonConvert.SerializeObject(response);
         }
@@ -108,5 +66,48 @@ namespace AiStudio4.InjectedDependencies
             await _webSocketServer.SendToClientAsync(clientId,
                 JsonConvert.SerializeObject(new { messageType = "cfrag", content = text }));
         }
+
+        private v4BranchedConversation LoadOrCreateConversation(string conversationId)
+        {
+            string conversationPath = Path.Combine(
+                Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+                "AiStudio4",
+                "conversations",
+                $"{conversationId}.json");
+
+            Directory.CreateDirectory(Path.GetDirectoryName(conversationPath));
+
+            if (File.Exists(conversationPath))
+            { 
+                var existingConversation = JsonConvert.DeserializeObject<v4BranchedConversation>(File.ReadAllText(conversationPath));
+                File.WriteAllText(conversationPath, JsonConvert.SerializeObject(existingConversation));
+                return existingConversation;
+            }
+            else
+            {
+                var newConversation = new v4BranchedConversation(conversationId)
+                {
+
+                };
+                File.WriteAllText(conversationPath, JsonConvert.SerializeObject(newConversation));
+                return newConversation;
+            }
+        }
     }
+
+    public class v4BranchedConversation
+    { 
+        public string ConversationId { get; internal set; }
+
+        public v4BranchedConversation()
+        {
+
+        }
+
+        public v4BranchedConversation(string conversationId)
+        {
+            ConversationId = conversationId;
+        }
+    }
+
 }
