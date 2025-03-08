f653dd2ce133f257c566e85a2e024f538dd9da21
Max <max.hedge@gmail.com>
2025-03-08 21:24:23 +0000

    Remove dead code.

--- AiStudio4/AiStudio4.Web/src/services/messaging/IMessageService.ts
+++
@@ -1,8 +0,0 @@
-export interface IMessageService {
-    connect(): Promise<void>;
-    disconnect(): Promise<void>;
-    sendMessage(message: string): Promise<void>;
-    subscribe(messageType: string, handler: (data: any) => void): void;
-    unsubscribe(messageType: string, handler: (data: any) => void): void;
-    isConnected(): boolean;
-}
