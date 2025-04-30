5184f66c775e6efe4133e05d7f26c470c4243248
Max <max.hedge@gmail.com>
2025-04-30 02:08:17 +0100

    Shatter conv view into pieces.

---
+++ AiStudio4/AiStudio4.Web/src/components/ConvView/index.ts
@@ -0,0 +1,9 @@
+﻿// AiStudio4.Web\src\components\ConvView\index.ts
+export * from './ConvView';
+export * from './MessageItem';
+export * from './MessageActions';
+export * from './MessageEditor';
+export * from './MessageMetadata';
+export * from './StreamingMessage';
+export * from './ConversationControls';
+export * from './ScrollManager';
