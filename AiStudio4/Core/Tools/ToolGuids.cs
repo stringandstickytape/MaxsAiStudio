49939338f59efc619e88d51acd7f5f8cdee0f18d
Max <max.hedge@gmail.com>
2025-06-05 23:33:41 +0100

    Refactor tools 1

---
+++ AiStudio4/Core/Tools/ToolGuids.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AiStudio4.Core.Tools
+{
+    static class ToolGuids
+    {
+        public const string AZURE_DEV_OPS_GET_COMMIT_DIFFS_TOOL_GUID = "c4d8e7f6-5a3b-2c1d-9e8f-7a6b5c4d3e2f";
+    }
+}
