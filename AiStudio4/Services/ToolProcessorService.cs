e57cb4deb95fe2e8938cb7cc765f2826f47fba89
Max <max.hedge@gmail.com>
2025-06-14 20:40:22 +0100

    AiStudio4: Refactor ToolProcessorService content block handling
    - Split tool response content into separate AiHidden and Tool content blocks in RequestBlocks
    - Change ResponseBlocks content type to AiHidden for tool run message

--- AiStudio4/Services/ToolProcessorService.cs
+++ AiStudio4/Services/ToolProcessorService.cs
@@ -189,7 +189,9 @@ namespace AiStudio4.Services
                             {
                                 //response.ContentBlocks.Add(new ContentBlock { Content = $"\n\n{toolResponse.ToolName}\n\n" });
 
-                                resultContentBlocks.RequestBlocks.Add(new ContentBlock { Content = $"{toolResponse.ToolName}\n{cleanedToolResponseText}\n", ContentType = ContentType.Tool });
+                                resultContentBlocks.RequestBlocks.Add(new ContentBlock { Content = $"{toolResponse.ToolName}\n", ContentType = ContentType.AiHidden });
+
+                                resultContentBlocks.RequestBlocks.Add(new ContentBlock { Content = $"{cleanedToolResponseText}\n", ContentType = ContentType.Tool });
                                 // tool already retrieved above
 
                                 var builtIn = _builtinToolService.GetBuiltinTools().First(x => x.Name == toolResponse.ToolName);
@@ -200,7 +202,7 @@ namespace AiStudio4.Services
                                 {
                                     toolResultMessageContent += $"{toolResponse.ToolName}";
 
-                                    resultContentBlocks.ResponseBlocks.Add(new ContentBlock { Content = $"Ran {toolResponse.ToolName}:\n\n" , ContentType = ContentType.Tool });
+                                    resultContentBlocks.ResponseBlocks.Add(new ContentBlock { Content = $"Ran {toolResponse.ToolName}:\n\n" , ContentType = ContentType.AiHidden });
                                 }
 
                                 if (!string.IsNullOrEmpty(builtinToolResult.ResultMessage))
